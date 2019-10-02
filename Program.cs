4bcd02162a2092e5247d20785ed974a9d7126fd7
neginadibi <55991862+neginadibi@users.noreply.github.com>
2019-10-01 22:13:09 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            string inputString;
+            //showing selection options
+            do
+            {
+                Console.WriteLine("1-Enter triangle dimensions");
+                Console.WriteLine("2-Exit");
+                Console.WriteLine("Enter number: ");
+                inputString = Console.ReadLine();
+            } while (!(int.TryParse(inputString, out input)) || input < 0 || input > 3);
+            
+
+            //taking inputs
+            if (input == 1)
+            {
+                Console.WriteLine("Enter number1");
+                int num1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter number2");
+                int num2 = int.Parse(Console.ReadLine());
+           
+                Console.WriteLine("Enter number3");
+                int num3 = int.Parse(Console.ReadLine());
+
+                string output = TriangleSolver.Analyze(num1, num2, num3);
+                Console.WriteLine(output);
+            }
+            //for exiting program
+            else
+            {
+                Environment.Exit(-1);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ TriangleSolver.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+
+    //TriangleSolver Class
+    public class TriangleSolver
+    {
+        //Analyze Method for checking whether it forms a triangle or not
+        public static string Analyze(int a, int b, int c)
+        {
+            //checking for equilateral triangle
+            if (a == b && b == c)
+            {
+                return "It is an equilateral triangle";
+            }
+            else
+            {
+                //checking for Isosceles triangle
+                if (a == b || b == c || a == c)
+                {
+                    return "It is an Isosceles triangle";
+                }
+                //checking for scalene and not a triangle
+                else
+                {
+                    if (a>b && a>c)
+                    {
+                        if((b+c)>a)
+                            return "It is a scalene triangle";
+                        else
+                            return "It is not a triangle";
+                    }
+
+                    else if (b > a && b > c)
+                    {
+                        if ((a + c) > b)
+                            return "It is a scalene triangle";
+                        else
+                            return "It is not a triangle";
+                    }
+
+                    else if (c > a && c > b)
+                    {
+                        if ((b + a) > c)
+                            return "It is a scalene triangle";
+                        else
+                            return "It is not a triangle";
+                    }
+                    return "It is not a triangle";
+
+
+                }
+            }
+        }
+    }
+}
